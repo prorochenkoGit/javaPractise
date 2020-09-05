3388a922599915d12f95dfd528245ce114f2c4e4
Student <proroch.enko04@yandex.ru>
2020-09-05 15:12:32 +0300

    init

---
+++ src/animal.java
@@ -0,0 +1,12 @@
+public class animal {
+    int a[];
+    int number;
+    int c;
+    int mass{ int a[]=new int a[];
+    }
+  // for(int i=0;i<5;i++){ a[i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры}
+//    }//
+
+}
+
+
---
+++ src/xlass2.java
@@ -0,0 +1,2 @@
+public class xlass2 {
+}
